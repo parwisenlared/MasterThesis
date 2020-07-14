8daba86b53a76b1cfb505f051d44ccd851b90719
Lucía Parga <luci.parga@hotmail.com>
2020-07-14 11:26:16 +0100

    update

--- MasterThesis/CNN.py
+++ MasterThesis/CNN.py
@@ -1,16 +1,24 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from matplotlib import pyplot
+from keras.datasets import cifar10
+from keras.utils import to_categorical
 
-
-
-model = keras.Sequential(
-    [
-        layers.Dense(2, activation="relu", name="layer1"),
-        layers.Dense(3, activation="relu", name="layer2"),
-        layers.Dense(4),
-    ]
-)
 
 if __name__ == "__main__":
-    print(model.layers)
+    # example of loading the cifar10 datase
+
+# load dataset
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
+
+# summarize loaded dataset
+    print('Train: X=%s, y=%s' % (trainX.shape, trainY.shape))
+    print('Test: X=%s, y=%s' % (testX.shape, testY.shape))
+
+# plot first few images (train images)
+    for i in range(9):
+	# define subplot
+	    pyplot.subplot(330 + 1 + i)
+	# plot raw pixel data
+	    pyplot.imshow(trainX[i])
+
+# show the figure
+    pyplot.show()
